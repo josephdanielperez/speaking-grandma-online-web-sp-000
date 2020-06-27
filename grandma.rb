7626d0e5a679ce3f3863bc9f7e6a7e11567c9e0b
Joseph Perez <hello@josephdanielperez.com>
2020-06-27 02:45:56 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,6 +2,8 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == "WHAT?"
+    return "NO, NOT SINCE 1938!"
   else phrase != "I LOVE YOU GRANDMA!"
     return "HUH?! SPEAK UP, SONNY!"
   end
